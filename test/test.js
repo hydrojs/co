5d21eaddcc75a897923c47ceecacd2ae2e4f3d3b
Jake Rosoman <jkroso@gmail.com>
2014-05-18 17:36:47 +1200

    remove unnecessary wrapper

--- test/test.js
+++ test/test.js
@@ -1,36 +1,34 @@
 var assert = require('assert');
 var Hydro = require('hydro');
 
-describe('hydro-co', function() {
-  it('handles GeneratorFunctions', function (done) {
-    var hydro = Hydro();
+it('handles GeneratorFunctions', function (done) {
+  var hydro = Hydro();
 
-    hydro.addSuite('test suite', function() {
-      hydro.addTest('generator', function *() {
-        var res = yield later();
-        assert.equal(res, 42);
-      });
-    });
-
-    hydro.run(function() {
-      assert.equal(hydro.tests()[0].status, 'passed');
-      done();
+  hydro.addSuite('test suite', function() {
+    hydro.addTest('generator', function *() {
+      var res = yield later();
+      assert.equal(res, 42);
     });
   });
 
-  it('catches errors in GeneratorFunctions', function (done) {
-    var hydro = Hydro();
+  hydro.run(function() {
+    assert.equal(hydro.tests()[0].status, 'passed');
+    done();
+  });
+});
 
-    hydro.addSuite('test suite', function() {
-      hydro.addTest('generator', function *() {
-        yield invalidFunction();
-      });
-    });
+it('catches errors in GeneratorFunctions', function (done) {
+  var hydro = Hydro();
 
-    hydro.run(function() {
-      assert.equal(hydro.tests()[0].status, 'failed');
-      done();
+  hydro.addSuite('test suite', function() {
+    hydro.addTest('generator', function *() {
+      yield invalidFunction();
     });
+  });
+
+  hydro.run(function() {
+    assert.equal(hydro.tests()[0].status, 'failed');
+    done();
   });
 });
 
